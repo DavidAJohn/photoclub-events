d2f0bec789181570ff04555ce0eee2b065fde399
David John <davidajohn@gmail.com>
2021-12-11 17:06:56 +0000

    Updated the login page with real controls and validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const formValues = this.loginForm.value;
+    const { email, password } = formValues;
+    //console.log(`Email: ${email}, Password: ${password}`);
+
+    this.authService.signIn({ identifier: email, password })
+      .subscribe(() => {
+
+      }
+    )
+    
   }
 
 }
